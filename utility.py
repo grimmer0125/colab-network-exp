a4eff026a8c83eda05abce82e2590509189e866e
Grimmer <grimmer0125@gmail.com>
2018-10-13 22:03:34 +0800

    Add condition to tell if the environment is Google's colab

--- utility.py
+++ utility.py
@@ -1,21 +1,21 @@
 import matplotlib
+import matplotlib.pyplot as plt
 
 def is_notebook():
     try:
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
+        shell = get_ipython().__class__.__name__ 
+        if shell == 'ZMQInteractiveShell': 
             print("in jupyter notebook")
-            return True   # Jupyter notebook or qtconsole
+            return True   # common Jupyter notebook or qtconsole
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
-        else:
+        elif shell == 'Shell' # Google's colab. # class: google.colab._shell
             return False  # Other type (?)
     except NameError:
         return False
 
 if is_notebook() == False:
     matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
 def show_train_history(train_history,train,validation):
     plt.plot(train_history.history[train])
